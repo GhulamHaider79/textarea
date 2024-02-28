68137d353c0b6420e8d9541596a640458e64651a
GhulamHaider79 <127304370+GhulamHaider79@users.noreply.github.com>
2024-02-28 18:08:24 +0500

    Text Area Assignment

--- app.js
+++ app.js
@@ -1,38 +1,35 @@
 function processInput() {
     var inputText = document.getElementById("textArea").value;
-    console.log(inputText)
-    var lines = inputText.split('\n');
-
-    var qaObject = {};
-
-    for (var i = 0; i < lines.length; i += 5) {
-        // Assuming each set of 5 lines corresponds to a single QA pair
-        var questionIndex = i;
-        
-        var option1Index = i + 1;
-        var option2Index = i + 2;
-        var option3Index = i + 3;
-        var option4Index = i + 4;
-
-        var question = lines[questionIndex].trim();
-        var option1 = lines[option1Index];
-        var option2 = lines[option2Index];
-        var option3 = lines[option3Index];
-        var option4 = lines[option4Index];
-
-        // Store the QA pair in the object
-        qaObject[question] = {
-            option1: option1,
-            option2: option2,
-            option3: option3,
-            option4: option4
-        };
-    }
-
-     console.log(qaObject);
-
+    var lines = inputText.split('\n')
     
-     document.getElementById("textArea").value = "";
-     alert("Submit Successfully  See in Console")
+    var qaObjects = {};
+   
+    
+
+
+    for (var i = 0; i < lines.length; i++) {
+        // console.log(lines[i].length-1);
+        // console.log();
+       var question = ''
+        var option = ''
+
+         if (lines[i].charAt(lines[i].length - 1) == "?") {
+            question = lines[i]
+        }else{
+
+            option = lines[i]
+        }
+         
+        qaObjects.question = question;
+        qaObjects.option =option
+       
+        //console.log(question);
+    }
+   
+    
+    
+    console.log(qaObjects);
 
 }
+
+
